79a1a98d6e387ee07fa27e7bdd7e2596003f806c
slaterdaniel <dslater2028@mka.org>
2026-04-29 19:16:02 -0400

    first commit

---
+++ style.js
@@ -0,0 +1,11 @@
+// const choices = document.querySelectorAll("choice")
+
+// let count = 1
+
+// choices.forEach(choice => {
+//     choice.addEventListener("click", () => {
+//         choice.
+
+
+//     });
+// });
